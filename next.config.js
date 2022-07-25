ce4dcc35ed16066c7f46ad7d29b59e7dc3a425fb
nitantchhajed <nitant.chhajed@outlook.com>
2022-07-25 11:26:49 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'openseauserdata.com',
+      'brand.assets.adidas.com',
+      'media0.giphy.com',
+      'avatars.dicebear.com',
+      'media1.giphy.com',
+      'media3.giphy.com',
+      'media2.giphy.com',
+      'media4.giphy.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { QueppelinProvider } from '../context/QueppelinContext'
+import { ModalProvider } from 'react-simple-hook-modal'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
+      <QueppelinProvider>
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
+      </QueppelinProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
+
+
